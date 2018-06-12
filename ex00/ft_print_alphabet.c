0dd8bda6442aae8b1081a63bcc27d7c3c704f205
davidcharlat <david_charlat@yahoo.fr>
2018-06-12 14:04:14 +0200

    ex00

---
+++ ex00/ft_print_alphabet.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+# include <unistd.h>
+int ft_putchar (char c)
+{
+	write (1,&c,1);
+	return 0;
+}
+
+void ft_print_alphabet (void)
+{
+	char tableau []="abcdefghijklmnopqrstuvwxyz";
+	int i=0;
+	for (i=0;i<26;i=i+1)
+		{
+		ft_putchar (tableau[i]);
+		}
+	ft_putchar ('\n');
+
+}
+
+int main()
+{
+	ft_print_alphabet();
+	return 0;
+}
